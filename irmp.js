c606a9bb131483778206bcb60f213f6c37867e5f
ryanbey <beycommaryan@gmail.com>
2021-11-05 12:33:32 -0600

    deleted main page content, added some basic html

--- irmp.js
+++ irmp.js
@@ -1,10 +1,20 @@
-// Remove old header
+// Remove existing stylesheets
+for (let i = 0; i < document.getElementsByTagName('link').length; i++) {
+    document.styleSheets[i].disabled = true;
+}
+
+// Let's start with a blank slate shall we
 document.querySelector('.container').remove();
+document.querySelector('table').remove();
+document.querySelector('#fixedFooter').remove();
+document.querySelector('#fader').remove();
+document.querySelector('body').style.removeProperty('background');
 
-// Below is all in a tr element, figure out how to delete that
-document.querySelector('.banner_img').remove();
-document.querySelector('#banner_img').remove();
-document.querySelector('#racepanel_wrapper').remove();
+// Add new HTML in probably the least professional way possible
+document.body.innerHTML =
+'<h1>Hello there</h1>' +
+'<h2>yo just testing things hbu</h2>' +
+'<p>yup thats cool hopefully it works</p>';
 
-// Remove sidebar ads
-document.querySelectorAll('.siteBGElement').forEach(e => e.remove());
+// trying to use external html file, not working yet, keep Googling
+$( body ).load( "directory/test.html" );
